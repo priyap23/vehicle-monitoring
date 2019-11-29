eca06fe9bbe2c56086545f1001ed08a800d4a106
priyap23 <priyaparate.parate0@gmail.com>
2019-11-29 20:32:15 +0100

    removed duplicate files

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,7 +1,7 @@
 var app = angular.module("VehicleManagement", []);
 
 //Controller Part
-app.controller("VehicleController", function($scope, $http) {
+app.controller("VehicleController", function($scope, $http,  $interval) {
  
  
     $scope.vehicles = [];
@@ -13,15 +13,16 @@ app.controller("VehicleController", function($scope, $http) {
     };
  
     // Now load the data from server
-    _refreshVehicleData();
-   
-   
- 
-    // Private Method  
-    // Call: http://localhost:8080/home
-    function _refreshVehicleData() {
-    	 var method = "";
-         var url = "";
+     _refreshVehicleData();
+    
+    _updateVehicleStatusData();
+    
+    _loadVehicleData();
+    
+    function _loadVehicleData() {
+        var auto = $interval(function() {
+       	 var method = "";
+            var url = "";
         $http({
             method: 'GET',
             url: '/home'
@@ -33,8 +34,49 @@ app.controller("VehicleController", function($scope, $http) {
                 console.log("Error: " + res.status + " : " + res.data);
             }
         );
+     }, 61000);
+     
+   }
+     
+        
+    //It loads the data once user hit URL
+    //Calls: http://localhost:8080/home
+    function _refreshVehicleData() {
+	 var method = "";
+     var url = "";
+     $http({
+        method: 'GET',
+        url: '/home'
+     }).then(
+        function(res) { // success
+            $scope.vehicles = res.data;
+        },
+        function(res) { // error
+            console.log("Error: " + res.status + " : " + res.data);
+            }
+      );
+  
     }
  
+    //It updates the status of vehicle after a minute
+    //Calls: http://localhost:8080/updateStatus
+    function _updateVehicleStatusData() {
+        var auto = $interval(function() {
+       	 var method = "";
+            var url = "";
+	        $http({
+	            method: 'GET',
+	            url: '/updateStatus'
+	        }).then(
+	              function(res) { // error
+	                console.log("Error: " + res.status + " : " + res.data);
+	            }
+	        );
+	       
+	     }, 60000);
+        
+      }
+    
     function _success(res) {
     	_refreshVehicleData();
         
@@ -47,6 +89,16 @@ app.controller("VehicleController", function($scope, $http) {
         var config = res.config;
         alert("Error: " + status + ":" + data);
     }
+    
+    //For changing the status color as per value
+    //For example: Disconnect : Red color, Connect: green color
+    $scope.getStatusClass = function (strVehicleStatus) {
+    	  if (strVehicleStatus == 'Connected') {
+    	    return 'text-success';
+    	  } else {
+    	    return 'text-danger';
+    	  }
+    	};
  
    
 });
